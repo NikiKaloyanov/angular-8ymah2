946c88577e6e8ede4ad3fbf60e8e6f5401183014
NikiKaloyanov <nikolaikaloianov@gmail.com>
2022-07-01 13:16:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8ymah2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8ymah2)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
+
+}
+
+export class ProductListComponent {
+
+  products = [...products];
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
